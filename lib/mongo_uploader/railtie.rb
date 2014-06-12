8857e34579ff67b0ef5c963e2f6ffeaee0c98958
Sandro Duarte <sandroduarte@tre-rs.gov.br>
2014-06-12 11:07:17 -0300

    log into its own log file

--- lib/mongo_uploader/railtie.rb
+++ lib/mongo_uploader/railtie.rb
@@ -3,7 +3,12 @@ module MongoUploader
   class Railtie < Rails::Railtie
 
     initializer 'Rails logger' do
-      MongoUploader.logger = Rails.logger
+      MongoUploader.logger = ::Logger.new(Rails.root.join('log/mongo_uploader.log'), 10, 1_048_576)
+
+      MongoUploader.logger.formatter = proc do |severity, datetime, progname, msg|
+        "[#{Process.pid}] #{datetime} - #{msg}\n"
+      end
+
     end
 
     initializer "mongo_uploader.use_middleware" do |app|
--- lib/mongo_uploader/storage.rb
+++ lib/mongo_uploader/storage.rb
@@ -5,7 +5,7 @@ module MongoUploader
   class Storage
 
     def initialize()
-      MongoUploader.logger.info "MONGO - Connecting to #{MongoUploader::Base.config.host}:#{MongoUploader::Base.config.db}"
+      MongoUploader.logger.info "CONNECT: #{MongoUploader::Base.config.host}:#{MongoUploader::Base.config.db}"
 
       @db = Mongo::Connection.new(MongoUploader::Base.config.host).db(MongoUploader::Base.config.db)
 
@@ -20,15 +20,19 @@ module MongoUploader
       _filename = _file.original_filename.sanitize
       _content_type = ::MIME::Types.type_for(_file.original_filename).first.to_s
 
+      MongoUploader.logger.debug "STORE: #{_filename} (#{_content_type})"
+
       @grid.put(_file, :content_type => _content_type, :filename => _filename)
     end
 
     def retrieve(_id)
+      MongoUploader.logger.debug "RETRIEVE: #{_id}"
+
       id = BSON::ObjectId.from_string(_id)
       @grid.get(id)
 
     rescue Mongo::GridFileNotFound => e
-      MongoUploader.logger.error "MONGO - #{e.message}"
+      MongoUploader.logger.error e.message
     end
 
     def delete(_id)
@@ -36,7 +40,7 @@ module MongoUploader
       @grid.delete(id)
 
     rescue Mongo::GridFileNotFound => e
-      MongoUploader.logger.error "MONGO - #{e.message}"
+      MongoUploader.logger.error e.message
     end
 
   end
